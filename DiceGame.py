b4c3335d37c1609ee0eee3196a8910201dec4be0
塩生裕希 <shionasuhiroshinozomi@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 13:11:53 +0900

    Third commit

--- DiceGame.py
+++ DiceGame.py
@@ -2,7 +2,14 @@ import random
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
 total = dice1 + dice2
+print("What is your name?")
+name = input(">")
+print("Hello, "+ name +"!")
 print("Rolling the dice...")
-print("Die 1: ",dice1)
-print("Die 2: ",dice2)
-print("Total value: ",total)
+print("Die 1: "+ str(dice1))
+print("Die 2: "+ str(dice2))
+print("Total value: "+ str(total))
+if total <= 7:
+  print("You lost")
+else:
+  print("You won")
