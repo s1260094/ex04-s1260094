6cadc5ffd27522e6b65ecc520f7354dc3d2852d9
塩生裕希 <shionasuhiroshinozomi@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 13:33:27 +0900

    Add one new feature

--- DiceGame.py
+++ DiceGame.py
@@ -1,15 +1,17 @@
 import random
 dice1 = random.randint(1, 6)
 dice2 = random.randint(1, 6)
-total = dice1 + dice2
+dice3 = random.randint(1, 6)
+total = dice1 + dice2 + dice3
 print("What is your name?")
 name = input(">")
 print("Hello, "+ name +"!")
 print("Rolling the dice...")
 print("Die 1: "+ str(dice1))
 print("Die 2: "+ str(dice2))
+print("Die 3: "+ str(dice3))
 print("Total value: "+ str(total))
-if total <= 7:
+if total <= 9:
   print(name +" lost")
 else:
   print(name +" won")
