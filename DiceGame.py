52f6c0fb786041c40b906803ce6ab6ddbd88974a
塩生裕希 <shionasuhiroshinozomi@wlan-napt-003.u-aizu.ac.jp>
2019-07-11 12:56:04 +0900

    First commit

---
+++ DiceGame.py
@@ -0,0 +1,8 @@
+import random
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
+total = dice1 + dice2
+print("Rolling the dice...")
+print("Die 1: ",dice1)
+print("Die 2: ",dice2)
+print("Total value: ",total)
